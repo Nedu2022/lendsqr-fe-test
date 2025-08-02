c80b1fef14875eb6cd13a32e28996ba71a42a018
Nedu2022 <nwabuokeichinedu00@gmail.com>
2025-08-02 13:46:56 +0100

    fix: authentication

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,11 @@
+import React, { type ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const isLoggedIn = localStorage.getItem('isLoggedIn');
   return isLoggedIn ? children : <Navigate to="/login" replace />;
 };
